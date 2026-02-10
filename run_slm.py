b2db36bcfec82113a477b86ac5aa87dd476079e1
Jun Kim <sk12164@nyu.edu>
2026-02-10 13:19:59 -0500

    Remove obsolete run_slm.py (initial SLM test script)

--- run_slm.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-pipe = pipeline("text-generation", model="distilgpt2")
-
-output = pipe("Hello UN AI Safety Lab", max_new_tokens=40)
-print(output[0]["generated_text"])
