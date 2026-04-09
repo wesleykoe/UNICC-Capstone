31772a91e79b3ce333622f3a8a48fbe9b44f2215
wesleykoe <wesleykoe123@gmail.com>
2026-04-09 15:24:30 -0400

    Update UNICC_Adapters_Training.py
    Updated token

--- Council_of_Experts/UNICC_Adapters_Training.py
+++ Council_of_Experts/UNICC_Adapters_Training.py
@@ -139,7 +139,7 @@ print(f"   Epochs:          {NUM_EPOCHS}")
 from google.colab import userdata
 import huggingface_hub
 
-hf_token = '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'
+hf_token = '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'
 huggingface_hub.login(token=hf_token)
 
 def load_base_model():
